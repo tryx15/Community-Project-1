b22e0da3a4cab873ae7646c7184b95e7e915c245
Tyrone <kevonx15@outlook.com>
2018-01-05 04:11:07 -0500

    4_Added Controller and views for the areas, deleted user area, created one main layout, modified routing

--- SProj_MtoM/App_Start/RouteConfig.cs
+++ SProj_MtoM/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace SProj_MtoM
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            (RouteTable.Routes[routes.Count - 1] as Route).DataTokens["area"] = "Common";
         }
     }
 }
---
+++ SProj_MtoM/Areas/Common/Controllers/OpportunityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SProj_MtoM.Areas.Common.Controllers
+{
+    public class OpportunityController : Controller
+    {
+        // GET: Common/Opportunity
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
